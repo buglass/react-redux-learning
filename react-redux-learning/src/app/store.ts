58cfad6a8683b66a839da29260a55b027009bb0a
adam <adam.buglass@emishealth.com>
2020-08-14 17:09:21 +0100

    Add internationalisation reducer.

--- react-redux-learning/src/app/store.ts
+++ react-redux-learning/src/app/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import internationalisationReducer from '../features/internationalisation/internationalisationSlice';
 
 // Add type support by getting the type of combineReducers.
 // Using combineReducers is a good idea anyway.
 const rootReducer = combineReducers({
-  counterReducer: counterReducer
+  counterReducer: counterReducer,
+  internationalisationReducer: internationalisationReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ react-redux-learning/src/features/internationalisation/internationalisationSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction, ThunkAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+export interface IState {
+  region: string;
+}
+
+const initialState: IState = {
+  region: "English"
+}
+
+export const internationalisationSlice = createSlice({
+  name: 'internationalisation',
+  initialState: initialState,
+  reducers: {
+    set: (state: IState, action: PayloadAction<string>) => {
+      state.region = action.payload;
+    }
+  }
+});
+
+export const { set } = internationalisationSlice.actions;
+
+export const incrementAsync = (region: string):
+  ThunkAction<void, ReturnType<typeof internationalisationSlice.reducer>, unknown, PayloadAction<string>> =>
+  async dispatch =>
+{
+  dispatch(set(region));
+};
+
+export const selectRegion = (state: RootState) => state.internationalisationReducer.region;
+
+export default internationalisationSlice.reducer;
