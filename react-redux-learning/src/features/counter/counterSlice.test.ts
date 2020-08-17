8cc1e1ab7878d9448fc89fde43cb9a904ab41ae9
adam <adam.buglass@emishealth.com>
2020-08-17 14:57:29 +0100

    Add tests for counterSlice reducer.

--- react-redux-learning/src/features/counter/counterSlice.test.ts
+++ react-redux-learning/src/features/counter/counterSlice.test.ts
@@ -1,10 +1,66 @@
-import counterReducer from "./counterSlice";
+import counterReducer, { decrement, increment, incrementAsync, incrementByAmount } from "./counterSlice";
 
 describe('counterSlice', () => {
   it('should initialise correctly', () => {
     expect(counterReducer(undefined, {})).toEqual(
       {
         counter: 0
+      }
+    );
+  });
+
+  it('should increment correctly', () => {
+    expect(counterReducer(undefined, increment, { counter: 0 })).toEqual(
+      {
+        counter: 1
+      }
+    );
+  });
+
+  it('should decrement correctly', () => {
+    expect(counterReducer(undefined, decrement, { counter: 0 })).toEqual(
+      {
+        counter: -1
+      }
+    );
+  });
+
+  it('should incrementByAmount of 0 correctly', () => {
+    expect(counterReducer(undefined, incrementByAmount(0), { counter: 0 })).toEqual(
+      {
+        counter: 0
+      }
+    );
+  });
+
+  it('should incrementByAmount of 1 correctly', () => {
+    expect(counterReducer(undefined, incrementByAmount(1), { counter: 0 })).toEqual(
+      {
+        counter: 1
+      }
+    );
+  });
+
+  it('should incrementByAmount of 2 correctly', () => {
+    expect(counterReducer(undefined, incrementByAmount(2), { counter: 0 })).toEqual(
+      {
+        counter: 2
+      }
+    );
+  });
+
+  it('should incrementByAmount of -1 correctly', () => {
+    expect(counterReducer(undefined, incrementByAmount(-1), { counter: 0 })).toEqual(
+      {
+        counter: -1
+      }
+    );
+  });
+
+  it('should incrementByAmount of -2 correctly', () => {
+    expect(counterReducer(undefined, incrementByAmount(-2), { counter: 0 })).toEqual(
+      {
+        counter: -2
       }
     );
   });
