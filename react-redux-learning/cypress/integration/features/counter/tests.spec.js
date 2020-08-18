30245219c2164bb84b505a16f0c0da141918bac7
adam <adam.buglass@emishealth.com>
2020-08-18 17:34:42 +0100

    Resolve alignment in test files

--- react-redux-learning/cypress/integration/features/counter/tests.spec.js
+++ react-redux-learning/cypress/integration/features/counter/tests.spec.js
@@ -1,42 +1,42 @@
 describe('counter tests', () => {
-    it('initialises to 0', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#counterValue').eq('0')
-    });
+  it('initialises to 0', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#counterValue').eq('0')
+  });
 
-    it('increments correctly', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#incrementCounter').click()
-        cy.get('#counterValue').should('have.text', '1')
-        cy.get('#incrementCounter').click()
-        cy.get('#counterValue').should('have.text', '2')
-    });
+  it('increments correctly', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#incrementCounter').click()
+    cy.get('#counterValue').should('have.text', '1')
+    cy.get('#incrementCounter').click()
+    cy.get('#counterValue').should('have.text', '2')
+  });
 
-    it('decrements correctly', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#decrementCounter').click()
-        cy.get('#counterValue').should('have.text', '-1')
-        cy.get('#decrementCounter').click()
-        cy.get('#counterValue').should('have.text', '-2')
-    });
+  it('decrements correctly', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#decrementCounter').click()
+    cy.get('#counterValue').should('have.text', '-1')
+    cy.get('#decrementCounter').click()
+    cy.get('#counterValue').should('have.text', '-2')
+  });
 
-    it('increments by specific amount correctly', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#incrementAmount').clear()
-        cy.get('#incrementAmount').type('3')
-        cy.get('#incrementByAmount').click()
-        cy.get('#counterValue').should('have.text', '3')
-    });
+  it('increments by specific amount correctly', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#incrementAmount').clear()
+    cy.get('#incrementAmount').type('3')
+    cy.get('#incrementByAmount').click()
+    cy.get('#counterValue').should('have.text', '3')
+  });
 
-    it('increments by specific amount using async correctly', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#incrementAmount').clear()
-        cy.get('#incrementAmount').type('3')
-        cy.get('#incrementByAmountAsync').click()
-        // Feels a bit hacky.
-        // Use contains to ensure a minimum timeout is allowed for the element to update.
-        // Then use should have text to ensure the precise text exists.
-        cy.contains('#counterValue', '3', {timeout: 11000})
-        cy.get('#counterValue').should('have.text', '3')
-    });
+  it('increments by specific amount using async correctly', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#incrementAmount').clear()
+    cy.get('#incrementAmount').type('3')
+    cy.get('#incrementByAmountAsync').click()
+    // Feels a bit hacky.
+    // Use contains to ensure a minimum timeout is allowed for the element to update.
+    // Then use should have text to ensure the precise text exists.
+    cy.contains('#counterValue', '3', {timeout: 11000})
+    cy.get('#counterValue').should('have.text', '3')
+  });
 })
