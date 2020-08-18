74a7131e0800b9f0c7bc92b927b9b505ebc66441
adam <adam.buglass@emishealth.com>
2020-08-18 17:32:06 +0100

    Add Cypress tests for CSS style properties

--- react-redux-learning/cypress/integration/features/internationalisation/tests.spec.js
+++ react-redux-learning/cypress/integration/features/internationalisation/tests.spec.js
@@ -1,20 +1,41 @@
 describe('internationalisation tests', () => {
     it('style initialises as english', () => {
         cy.visit('http://localhost:3000')
+
+        // The style is set in the reducer so could be tested functionally but should arguably use visual testing for this.
         cy.get('#internationalisationIndicatorDiv').should('have.class', 'englishStyle')
+
+        // A POC of testing the CSS but this is fragile and should be done using a proper visual testing tool.
+        // https://docs.cypress.io/guides/tooling/visual-testing.html#Functional-vs-visual-testing
+        cy.get('#internationalisationIndicatorDiv').should('have.css', 'color', 'rgb(255, 0, 0)')
+        cy.get('#internationalisationIndicatorDiv').should('have.css', 'background-color', 'rgb(255, 255, 255)')
     });
 
     it('style changes to english', () => {
         cy.visit('http://localhost:3000')
         cy.get('#selectCymraeg').click()
         cy.get('#selectEnglish').click()
+
+        // The style is set in the reducer so could be tested functionally but should arguably use visual testing for this.
         cy.get('#internationalisationIndicatorDiv').should('have.class', 'englishStyle')
+
+        // A POC of testing the CSS but this is fragile and should be done using a proper visual testing tool.
+        // https://docs.cypress.io/guides/tooling/visual-testing.html#Functional-vs-visual-testing
+        cy.get('#internationalisationIndicatorDiv').should('have.css', 'color', 'rgb(255, 0, 0)')
+        cy.get('#internationalisationIndicatorDiv').should('have.css', 'background-color', 'rgb(255, 255, 255)')
     });
 
     it('style changes to welsh', () => {
         cy.visit('http://localhost:3000')
         cy.get('#selectEnglish').click()
         cy.get('#selectCymraeg').click()
+
+        // The style is set in the reducer so could be tested functionally but should arguably use visual testing for this.
         cy.get('#internationalisationIndicatorDiv').should('have.class', 'cymraegStyle')
+
+        // A POC of testing the CSS but this is fragile and should be done using a proper visual testing tool.
+        // https://docs.cypress.io/guides/tooling/visual-testing.html#Functional-vs-visual-testing
+        cy.get('#internationalisationIndicatorDiv').should('have.css', 'color', 'rgb(255, 255, 255)')
+        cy.get('#internationalisationIndicatorDiv').should('have.css', 'background-color', 'rgb(255, 0, 0)')
     });
 });
